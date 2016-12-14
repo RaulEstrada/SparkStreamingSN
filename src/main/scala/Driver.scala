96345a8610d71fdab8e87fbcbee1d9729efcd1aa
RaulEstrada <raulestradaballesteros@gmail.com>
2016-12-14 10:02:30 +0100

    Created DataStream that reads new tweets every 20 seconds, filters those who contain a given hashtag passed as parameter to the Driver program, and prints them

--- src/main/scala/Driver.scala
+++ src/main/scala/Driver.scala
@@ -9,9 +9,19 @@ object Driver {
     val SECONDS = 20
 
     def main(args: Array[String]) {
+        if (args.length < 1) {
+            println("SocialMediaStream Driver usage: <word>")
+            System.exit(-1)
+        }
         val conf = new SparkConf()
         val context = new StreamingContext(conf, Seconds(SECONDS))
         val twitterStream = TwitterUtils.createStream(context, None)
+        val hashTag = "#" + args(0).toLowerCase()
+        val tweets = twitterStream.map(status => status.getText())
+            .filter(text => text.toLowerCase() contains hashTag)
+        tweets.print()
+        context.start()
+        context.awaitTermination()
     }
 
 }
